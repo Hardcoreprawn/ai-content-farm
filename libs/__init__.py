243e65b9358c9ff8da196e8bde58d506989e8369
James Brewster <35832945+Hardcoreprawn@users.noreply.github.com>
2025-08-25 21:41:49 +0000

    test: trigger pipeline for Azure OIDC authentication fix
    - Update comment to test the Terraform Azure authentication fix
    - Tests both Terraform Docker containerization and OIDC auth setup

--- libs/__init__.py
+++ libs/__init__.py
@@ -8,4 +8,4 @@ Version: 1.2.2 - Parallel container builds implementation (Aug 22, 2025)
 """
 
 __version__ = "1.2.2"
-# Trigger pipeline for Terraform Docker fix test
+# Test Azure OIDC authentication fix for Terraform
